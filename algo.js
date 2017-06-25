adf45f6d6198b0e7ca616653fae6f06c26b2d39e
Andre <[andre_w3@yahoo.com]>
2017-06-24 22:23:06 -0700

    new add

--- algo.js
+++ algo.js
@@ -1,3 +1,17 @@
+
+// print 1-255 ********
+
 for (var x = 1; x <= 255; x++){
 console.log (x);
+}
+
+// odd numbers ********
+for (var x = 1; x <= 1000; x= x+2){
+  console.log(x);
+}
+        // or
+for (var x = 0; x<= 1000; x++){
+  if(x%2==1){
+    console.log(x);
+  }
 }
