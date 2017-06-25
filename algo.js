10a69ca1eb1bb208b989b651d9beb9d3aae0e0b9
Andre <[andre_w3@yahoo.com]>
2017-06-24 23:52:16 -0700

    new algo

--- algo.js
+++ algo.js
@@ -15,3 +15,10 @@ for (var x = 0; x<= 1000; x++){
     console.log(x);
   }
 }
+
+// sum of all odd 1 - 5000**********
+var sum =0;
+for(var i=1; i<5000; i=i+2){
+  sum = sum + i;
+}
+console.log(sum);
