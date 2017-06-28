14b562552d8afdf4d6a2adbcbebebebe83851f39
Andre <[andre_w3@yahoo.com]>
2017-06-27 19:42:09 -0700

    odd arrays

--- algo.js
+++ algo.js
@@ -24,6 +24,14 @@ for(var i=1; i<5000; i=i+2){
 }
 console.log(sum);
 
+var sum =0;
+for(var i=0; i<5000; i++){
+  if (i%2==1);
+  sum = sum + i;
+}
+console.log(sum);
+
+
 // iterate array***********
 
 var x = [1,21,3,4,5];
@@ -34,7 +42,7 @@ console.log(x);
 // max***************
 var arr=[-3, 3, 5, 70, 10];
 var max= arr[0];
-for (var x=1;x<arr.length;x++){
+for (var x=1; x<arr.length; x++){
     if(arr[x] > max){                    //if(3 > -3)    => if(arr[0] > max)
         max = arr[x];                    // max = arr[1] => max = 3
     }
@@ -49,3 +57,21 @@ for (var x=0;x<arr.length;x++){
     sum = sum + arr[x]        //sum += arr[x]    => sum = 16 + arr[4] => sum = 16 + 20 => sum = 36
 }
 console.log(sum/arr.length);    // 36/5
+
+// array odd numbers****************
+var y =[];
+for(var i=1; i<=255; i= i+2)
+{
+  y.push(i);
+}
+console.log(y);
+
+
+
+// var y = [];
+// for (var x=1;x<=256;x++){
+//   if(x%2==1){
+//     y.push(x);
+//     console.log(x);
+//   }
+// }
