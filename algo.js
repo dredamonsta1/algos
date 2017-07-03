03ef225013f3bcb48934ac2bebe16ad961696e04
Andre <[andre_w3@yahoo.com]>
2017-07-03 12:53:15 -0700

    swapping algos

--- algo.js
+++ algo.js
@@ -176,3 +176,14 @@ for (var i=0; i<10; i++)
   arr.push(random_int);
 }
 console.log(arr);
+
+// swapping two values***************************
+var x = [2, 3, 5, 7, 6];
+var first = x[0];
+var last = arr.length - 1;
+var temp = x[0];{
+x[0] = x[x.length -1];
+x[x.length-1] = temp;
+
+}
+console.log(x);
